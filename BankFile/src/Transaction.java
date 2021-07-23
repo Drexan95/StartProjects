f222d60fa230ac56c188e2cb84f20ab9521e8b33
Drexan95 <86364162+Drexan95@users.noreply.github.com>
2021-07-23 18:05:20 +0700

    Delete BankFile directory

--- BankFile/src/Transaction.java
+++
@@ -1,9 +0,0 @@
-public class Transaction {
-    public String accountType, accountNumber, currency, date, reference, descritpion, income, outcome;
-
-    @Override
-    public String toString(){
-        return accountType+"\t" + accountNumber+"\t" +currency+"\t" + date +"\t"+ reference+"\t" +descritpion +"\t"+income+"\t" +outcome+"\n";
-    }
-}
-
