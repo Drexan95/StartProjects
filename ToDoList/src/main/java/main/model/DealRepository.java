91465f138bf83f2d873f76c0bead19854fc9194a
Drexan95 <86364162+Drexan95@users.noreply.github.com>
2021-09-05 15:39:23 +0700

    Delete ToDoList directory

--- ToDoList/src/main/java/main/model/DealRepository.java
+++
@@ -1,8 +0,0 @@
-package main.model;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DealRepository extends CrudRepository<Deal,Integer> {
-}
